4d68b21d7484e91387c52dca93ce63b222008692
Fano <ayushyush100@gmail.com>
2025-06-05 14:17:34 +0545

    done a few bit here and there read the rest in the devlogs

--- AIshit/genAi.py
+++ AIshit/genAi.py
@@ -1,7 +1,8 @@
 from PIL import Image
 from google import genai
 
-key = 'your_key'
+key = 'ayush'
+
 client = genai.Client(api_key=key)
 
 models = ['gemini-2.0-flash-exp','gemini-1.5-flash', 'gemini-2.0-flash', 'gemini-1.0-pro','gemini-1.5-pro']
